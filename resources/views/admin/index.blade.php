073c184a4bc6e59b7aed7df0aa4dc4f5700dfec0
finorosatabitha <finorosatabitha2118@gmail.com>
2026-04-13 08:38:42 +0700

    icon ngtma

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -493,28 +493,28 @@
                                 'label' => 'Government',
                                 'sub' => 'New GTMA Government',
                                 'badge' => 'GOV',
-                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>'
+                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 21h18M3 10h18M3 7l9-4 9 4M4 10h1v11H4zm6 0h1v11h-1zm5 0h1v11h-1zm5 0h1v11h-1z"/>'
                             ],
                             [
                                 'key' => 'private',
                                 'label' => 'Private',
                                 'sub' => 'New GTMA Private',
                                 'badge' => 'PVT',
-                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>'
+                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>'
                             ],
                             [
                                 'key' => 'soe',
                                 'label' => 'SOE',
                                 'sub' => 'New GTMA SOE',
                                 'badge' => 'SOE',
-                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>'
+                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>'
                             ],
                             [
                                 'key' => 'sme',
                                 'label' => 'SME',
                                 'sub' => 'New GTMA SME',
                                 'badge' => 'SME',
-                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>'
+                                'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"/>'
                             ],
                         ];
                     @endphp
